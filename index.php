a8e585a80c6878aa2cbdb11c1b00c8f703614c20
Iván Károly <karoly.ivan@rsoe.hu>
2016-10-03 08:54:05 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+    <head>     
+        <title>Sticky Footer</title>
+        <meta charset="UTF-8">
+        <link rel='stylesheet' type='text/css' href='style.css'>
+       
+    </head>
+<body>      
+    <!-- Site -->
+    <div class="wrapper">
+        <!-- Header -->
+        <div class="site header">
+            Header
+        </div>
+        
+        <!-- Content -->
+        <div class='site content'>  
+            <?php include 'content.php'; ?>
+        </div> 
+    
+        <!-- Footer -->
+        <div class="site footer">
+            Footer
+        </div>    
+    </div>  
+</body>
+</html>
