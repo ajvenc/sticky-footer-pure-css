e4a585dc9e191e0a7c2680bc041d2f711362c51e
Iván Károly <karoly.ivan@rsoe.hu>
2016-10-03 11:04:39 +0200

    Comments added and fixed

--- index.php
+++ index.php
@@ -5,27 +5,27 @@
         <meta charset="UTF-8">
         <link rel='stylesheet' type='text/css' href='style.css'>
     </head>
-<body>      
-    <!-- Wrapper -->
-    <div class="wrapper">
-        <!-- Header -->
-        <div class="site header">
-            Header
-        </div>
-        
-        <!-- Content -->
-        <div class='site content'>  
-            <!-- 
-                By writting the ?content GET variable to the end of URL you
-                can test it with long content too.
-            -->
-            <?php include 'content.php'; ?>
-        </div> 
-    
-        <!-- Footer -->
-        <div class="site footer">
-            Footer
-        </div>    
-    </div>  
-</body>
+    <body>      
+        <!-- Wrapper -->
+        <div class="wrapper">
+            <!-- Header -->
+            <div class="site header">
+                Header
+            </div>
+
+            <!-- Content -->
+            <div class='site content'>  
+                <!-- 
+                    By writting the ?content GET variable to the end of URL you
+                    can test it with long content too.
+                -->
+                <?php include 'content.php'; ?>
+            </div> 
+
+            <!-- Footer -->
+            <div class="site footer">
+                Footer
+            </div>    
+        </div>  
+    </body>
 </html>
